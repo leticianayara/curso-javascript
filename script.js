330355f42e34410efcf7a2e04f26159363ccb285
Leticia Costa <leticianayaralima@gmail.com>
2025-03-12 17:15:14 -0300

    Convertendo String para Number

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-// Static or Dynamic
+//Convertions or Casting
 
-//Static
-//C, C++, Java, TypeScript
-let lastName: string = 'Costa'
+let age = '35'
+console.log(age, typeof age)
 
-//Dynamic
-//Python, Javascript, Ruby. PHP
-let firstName = 'Leticia'
-let age = 35
+//ParseInt (inteiro)
+age = parseInt(age)
+console.log(age, typeof age)
+
+//ParseFloat (fracionario)
+age = '35.2'
+age = parseFloat(age)
+console.log(age, typeof age)
+
+//Unário (+)
+age = '35.2'
+age = +age
+console.log(age, typeof age)
+
+//Number (numero)
+age = '35.24'
+age = Number(age)
+console.log(age, typeof age)
