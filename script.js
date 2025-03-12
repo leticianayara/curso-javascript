5a4a8860a50b96e13e97fd1ecf0737544d0cdde0
Leticia Costa <leticianayaralima@gmail.com>
2025-03-12 16:47:09 -0300

    Linguagens estaticas e dinamicas

--- script.js
+++ script.js
@@ -1,49 +1,10 @@
-//Variables - var let const
-// camelCase
+// Static or Dynamic
 
-//Data type
-//Primitive Data Types
+//Static
+//C, C++, Java, TypeScript
+let lastName: string = 'Costa'
 
-//String
-let firsName = 'Lele'
-console.log(firsName, typeof firsName)
-
-//Number
+//Dynamic
+//Python, Javascript, Ruby. PHP
+let firstName = 'Leticia'
 let age = 35
-console.log(age, typeof age)
-
-//Boolean
-let yesNo = true
-console.log(yesNo, typeof yesNo)
-
-//Null
-let address = null
-console.log(address, typeof address)
-
-//Undefined
-let color
-console.log(color, typeof color)
-
-//Symbol
-let id = Symbol('id')
-let meuObjeto = {
-  [id]: 123456,
-}
-console.log(meuObjeto[id], typeof meuObjeto[id])
-
-//Data type
-//Reference Data Types
-
-//Arrays
-let numbers = [10, 20, 30, 35, 50]
-console.log(numbers)
-
-//Function
-function myName() {
-  console.log('Leticia')
-}
-console.log(myName, typeof myName)
-
-// Date
-let now = new Date()
-console.log(now)
