a39ec23aac57a557eac663fa0e93f2ed1aaf465f
Leticia Costa <leticianayaralima@gmail.com>
2025-03-12 17:40:47 -0300

    Converter Number para Boolean

--- script.js
+++ script.js
@@ -3,12 +3,12 @@
 let age = 35
 console.log(age, typeof age)
 
-//Number para String
-//toString
-age = age.toString()
+//Number para Boolean (true ou false)
+//Number 0 = false
+//Number 1... = true
+age = Boolean(age)
 console.log(age, typeof age)
 
-//String()
-age = 35
-age = String(age)
+age = 0
+age = Boolean(age)
 console.log(age, typeof age)
