1363087e268102bfa5e7d1f21c798d47b2e0cf54
Leticia Costa <leticianayaralima@gmail.com>
2025-03-13 09:52:04 -0300

    Operadores Aritmeticos Basicos e Complexos

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
-//Convertions or Casting
+//Operadores Aritméticos
 
-let age = 35
-console.log(age, typeof age)
+//Adição
+let total = 6 + 6
+console.log('adicao: ' + total)
 
-//Number para Boolean (true ou false)
-//Number 0 = false
-//Number 1... = true
-age = Boolean(age)
-console.log(age, typeof age)
+//Subtração
+total = 6 - 2
+console.log('subtracao: ' + total)
 
-age = 0
-age = Boolean(age)
-console.log(age, typeof age)
+//Multiplicação
+total = 6 * 2
+console.log('multiplicacao: ' + total)
+
+//Divisão
+total = 6 / 2
+console.log('divisão: ' + total)
+
+let num1 = 6
+let num2 = 6
+let totalSoma = num1 + num2
+console.log('totalSoma variaveis: ' + totalSoma)
+
+console.log('soma no console: ' + (num1 + num2))
+
+//Resto da divisao
+total = 4 % 3
+console.log('Resto da divisao: ' + total)
+
+//Incremento
+total = 5
+total++
+console.log('Incremento: ' + total)
+
+//Decremento
+total = 5
+total--
+console.log('Decremento: ' + total)
+
+//Exponencial
+total = 2 ** 3
+console.log('Exponencial: ' + total)
