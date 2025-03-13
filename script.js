2e8fa846048531939b0904bf6e279653ffc17eb1
Leticia Costa <leticianayaralima@gmail.com>
2025-03-13 17:03:35 -0300

    Configurando os metodos de Date()

--- script.js
+++ script.js
@@ -2,9 +2,12 @@
 
 let data = new Date()
 
+//data.setDate(9)
 console.log(data.getDate()) //dia do mes
 console.log(data.getDay()) // dia da semana (0-6), onde 0 e domingo
+//data.setMonth(0)
 console.log(data.getMonth()) // mes (0-11) onde 0 e Janeiro
+//data.setFullYear(2022)
 console.log(data.getFullYear()) // ano
 console.log(data.getHours()) // hora
 console.log(data.getMinutes()) // minutos
