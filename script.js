4b87ff3fe8e78dd65c4d67c34b7fbcdb31a5914a
Leticia Costa <leticianayaralima@gmail.com>
2025-03-13 16:22:37 -0300

    Criando Objetos

--- script.js
+++ script.js
@@ -1,41 +1,15 @@
-//Metodos Matematicos (Math)
+// Objetos (Multiplos valores) - Tesla
 
-let num1 = 2.5
-console.log(num1)
+let carName = 'Cybertruck'
+let carRange = 340
+let carMaxSpeed = 112
+let carHorsePower = 600
 
-//round - retorna o numero inteiro, sem casas decimais e
-//          seguindo as regras naturais (acimaa de 5
-//          arredonda pra cima)
-console.log('round: ' + Math.round(num1))
+let car = {
+  carName: 'Cybertruck',
+  carRange: 340,
+  carMaxSpeed: 112,
+  carHorsePower: 600,
+}
 
-//ceil - arredonda sempre pra cima
-num1 = 2.1
-console.log(num1)
-console.log('ceil: ' + Math.ceil(num1))
-
-//floor - arredonda sempre pra baixo
-num1 = 2.9
-console.log(num1)
-console.log('floor: ' + Math.floor(num1))
-
-//sqrt - raiz quadrada
-num1 = 14
-console.log(num1)
-console.log('sqrt: ' + Math.sqrt(num1))
-
-//pow - um numero x elevado a potencia y
-num1 = 2
-console.log(num1)
-console.log('pow: ' + Math.pow(num1, 3))
-
-//abs - retorna o valor absoluto
-num1 = -2
-console.log(num1)
-console.log('abs: ' + Math.abs(num1))
-
-//randow - retorna um numero aleatorio
-num1 = Math.random() * 10 + 1
-console.log('randow: ' + num1)
-
-//Misturando metodos
-console.log('round + randow: ' + Math.round(Math.random() * 100 + 1))
+console.log(car)
