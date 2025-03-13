e7e3543679729184d955af6c709c57db303dd864
Leticia Costa <leticianayaralima@gmail.com>
2025-03-13 16:54:04 -0300

    Os metodos do Date()

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 //Objeto Data e Hora (Date)
 
-let agora = new Date()
-console.log(agora)
+let data = new Date()
 
-let dataEspecifica = new Date(2025, 4, 20, 10, 35, 0)
-console.log(dataEspecifica)
-
-let dataString = new Date('2024/10/20 10:35:00')
-console.log(dataString)
+console.log(data.getDate()) //dia do mes
+console.log(data.getDay()) // dia da semana (0-6), onde 0 e domingo
+console.log(data.getMonth()) // mes (0-11) onde 0 e Janeiro
+console.log(data.getFullYear()) // ano
+console.log(data.getHours()) // hora
+console.log(data.getMinutes()) // minutos
+console.log(data.getSeconds()) // segundos
+console.log(data.getMilliseconds()) // milisegundos
