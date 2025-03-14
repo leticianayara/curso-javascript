b4330866b452d307ad5b68b86dc887b8951fa4ef
Leticia Costa <leticianayaralima@gmail.com>
2025-03-14 08:58:05 -0300

    Modificando e alterando elementos de um Array

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Arrays
+//Arrays (listas)
 
 let cart = ['Agua', 'Arroz', 'Carne', 'Feijão']
 console.log(cart)
@@ -9,3 +9,12 @@ console.log(cart[3])
 console.log(`A minha comida favorita é ${cart[1]}, ${cart[3]} e ${cart[2]} `)
 
 console.log(`A minha lista de compras para hoje contém os itens: ${cart}`)
+
+//<nome do campo>[<numero do campo>] - Qual o index do elemento do item que eu quero alterar
+let numbers = [10, 20, 33, 40, 5, 13, true]
+console.log(numbers[3])
+console.log(numbers[0] + numbers[2])
+console.log(numbers)
+console.log(cart)
+cart[1] = 'Laranja'
+console.log(cart)
