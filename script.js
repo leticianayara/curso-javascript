35529ac5d3b69487e0a624c67738d08ca5524f7e
Leticia Costa <leticianayaralima@gmail.com>
2025-03-14 11:41:49 -0300

    Concatenando listas

--- script.js
+++ script.js
@@ -1,29 +1,19 @@
-//Arrays (Nesting)
+//Arrays (Concatenate)
 
 let cart = ['Agua', 'Carne', 'Arroz', 'Feijão']
 let numbers = [10, 20, 33, 40, 5, 13, true]
+let cartNumbers = [cart, numbers]
 let y
 
-// pega um indice da tabela e add  novo item
-// (exemplo: criou um novo item na lista ja existende,
-// sendo este item um array)
-cart.push(numbers)
-console.log(cart)
+//Nesting
+//cart.push(numbers)
+//console.log(cart)
 
-//get a Carne
-y = cart[1]
-console.log(y)
+//Concatenate - coloco os itens de um array , dentro de outro array
+y = cart.concat(numbers)
 
-//get o 20
-y = cart[4][1]
-console.log(y)
+//console.log(cartNumbers)
+//y = cartNumbers[0][1]
+//y = cartNumbers[1][3]
 
-//get cartNmbers
-// cria uma nova tabela, com cada item sendo um novo item
-/// (exemplo: criou 2 itens, sendo cada item um arrays)
-let cartNumbers = [cart, numbers]
-console.log(cartNumbers)
-y = cartNumbers[0][1]
-console.log(y)
-y = cartNumbers[1][3]
 console.log(y)
