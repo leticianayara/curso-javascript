8395fd61d3a0014d6c0dfb90bc63c8f739c25d2c
Leticia Costa <leticianayaralima@gmail.com>
2025-03-14 10:44:04 -0300

    Realizando Chain com metodos

--- script.js
+++ script.js
@@ -1,50 +1,11 @@
 //Arrays (Metodos)
 
-let cart = ['Agua', 'Arroz', 'Carne', 'Feijão']
+let cart = ['Agua', 'Carne', 'Arroz', 'Feijão']
 let numbers = [10, 20, 33, 40, 5, 13, true]
 let y
 
-//includes() - verifica se existe ou nao e é casesensitive
-y = cart.includes('Agua')
-console.log(y)
+console.log(cart)
 
-y = cart.includes('Aguas')
-console.log(y)
-
-y = cart.includes('aguas')
-console.log(y)
-
-//indexOf() - retorna o indice onde esta o que vc procura,
-// -1 qnd nao acha e é casesensitive
-y = cart.indexOf('Arroz')
-console.log(y)
-
-y = cart.indexOf('Feijão')
-console.log(y)
-
-y = cart.indexOf('Feijao')
-console.log(y)
-
-//slice() - faz um corte do indice a ate o indice b,
-//  se passa só um indice deduz que vai o indice a ate
-// o final do array; qnd mostra os dois indice, ele conta
-// o indice a mas não conta o b
-// Não altera a variavel (cart por exemplo)
-y = cart.slice(1)
-console.log(y)
-
-y = cart.slice(1, 3)
-console.log(y)
-
-//splice() - faz um corte do indice a ate o indice b,
-//  se passa só um indice deduz que vai o indice a ate
-// o final do array; qnd mostra os dois indice, ele conta
-// o indice a e o indice b
-// Altera a variavel (cart por exemplo)
-cart = ['Agua', 'Arroz', 'Carne', 'Feijão']
-y = cart.splice(1)
-console.log(y)
-
-cart = ['Agua', 'Arroz', 'Carne', 'Feijão']
-y = cart.splice(1, 3)
+//y = cart.splice(1, 3).sort()
+y = cart.splice(1, 3).reverse().toString().includes('Carne')
 console.log(y)
