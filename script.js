0dde6b6d5da16d7e983c4fb82f05939149697aed
Leticia Costa <leticianayaralima@gmail.com>
2025-03-14 10:27:42 -0300

    Slice e Splice do JavaScript

--- script.js
+++ script.js
@@ -2,25 +2,49 @@
 
 let cart = ['Agua', 'Arroz', 'Carne', 'Feijão']
 let numbers = [10, 20, 33, 40, 5, 13, true]
+let y
 
-//Add um item na lista
-//cart[4] = 'Suco'
-cart.push('Suco') //add um item na lista
-//console.log(cart.length)
-console.log(cart)
+//includes() - verifica se existe ou nao e é casesensitive
+y = cart.includes('Agua')
+console.log(y)
 
-//Remover um item da lista
-cart.pop()
-console.log(cart)
+y = cart.includes('Aguas')
+console.log(y)
 
-//Remove o primeiro item da lista
-cart.shift()
-console.log(cart)
+y = cart.includes('aguas')
+console.log(y)
 
-//Add o item ao inicio da lista
-cart.unshift('Suco')
-console.log(cart)
+//indexOf() - retorna o indice onde esta o que vc procura,
+// -1 qnd nao acha e é casesensitive
+y = cart.indexOf('Arroz')
+console.log(y)
 
-//Coloca a lista em ordem alfabetica (ou numerica)
-cart.sort()
-console.log(cart)
+y = cart.indexOf('Feijão')
+console.log(y)
+
+y = cart.indexOf('Feijao')
+console.log(y)
+
+//slice() - faz um corte do indice a ate o indice b,
+//  se passa só um indice deduz que vai o indice a ate
+// o final do array; qnd mostra os dois indice, ele conta
+// o indice a mas não conta o b
+// Não altera a variavel (cart por exemplo)
+y = cart.slice(1)
+console.log(y)
+
+y = cart.slice(1, 3)
+console.log(y)
+
+//splice() - faz um corte do indice a ate o indice b,
+//  se passa só um indice deduz que vai o indice a ate
+// o final do array; qnd mostra os dois indice, ele conta
+// o indice a e o indice b
+// Altera a variavel (cart por exemplo)
+cart = ['Agua', 'Arroz', 'Carne', 'Feijão']
+y = cart.splice(1)
+console.log(y)
+
+cart = ['Agua', 'Arroz', 'Carne', 'Feijão']
+y = cart.splice(1, 3)
+console.log(y)
