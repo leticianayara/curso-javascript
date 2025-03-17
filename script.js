298aab4059a01b21868a65b1822e83bebdc8ed3e
Leticia Costa <leticianayaralima@gmail.com>
2025-03-17 11:32:04 -0300

    Entendendo o Truth e o Falsy

--- script.js
+++ script.js
@@ -1,32 +1,16 @@
-/**
- * Criar uma solução com o switch que retorne:
- * hora < 12 : Bom dia
- * hora < 18 : Boa tarde
- * hora >= 18 : Boa noite
- * A hora deve ser coletada do sistema local
- */
+// Truthy e Falsy (IF ELSE, Loop, Ope Logicos)
 
-let agora = new Date()
-//let agora = new Date(2030, 1, 8, 19, 0, 0)
+let texto = 'Andre' //Truthy
+//let texto = '' //Falsy
+// let texto = 0 //Falsy
+//let texto = null //Falsy
+//let texto = undefined //Falsy
+//let texto = NaN //Falsy
 
-let horas = agora.getHours()
+//qq coisa que seja um conteudo, sera Truthy
 
-// if (horas < 12) {
-//   console.log('Bom dia')
-// } else if (horas < 18) {
-//   console.log('Boa tarde')
-// } else {
-//   console.log('Boa noite')
-// }
-
-switch (true) {
-  case horas < 12:
-    console.log('Bom dia')
-    break
-  case horas < 18:
-    console.log('Boa tarde')
-    break
-  default:
-    console.log('Boa noite')
-    break
+if (texto) {
+  console.log('Truthy')
+} else {
+  console.log('Falsy')
 }
