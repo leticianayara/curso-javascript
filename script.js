d69d9e01ddd87148a110f2ea2f1b2c38d8b125e6
Leticia Costa <leticianayaralima@gmail.com>
2025-03-17 16:20:34 -0300

    Desafio JavaScript - Objetos Aninhados

--- script.js
+++ script.js
@@ -1,43 +1,30 @@
-// Objects and Key Pairs (Dictionary)
-// Arrray = Objects
+// Objetos, nested, Array
 
-// let ouraAndreInfo2 = [9.7, 7.53, '86%', 59, 84]
-
-const ouraAndreInfo = {
-  totalSleep: 7.53,
-  timeInBed: 9.7,
-  sleepEfficiency: '86%',
-  restinHeartRate: { maxHeartRate: 81, minHeartRate: 59 },
-  sleepScore: 84,
-  notes: ['Coffee', '30 Minutes reading'],
+let biblioteca = {
+  fantasy: [
+    {
+      anoPublicaca: 1954,
+      autor: 'J.R.R. Tolkien',
+      titulo: 'O Senhor dos Anéis',
+    },
+    {
+      autor: 'J.K. Rowling',
+      anoPublicaca: 1997,
+      titulo: 'Harry Potter',
+    },
+  ],
+  ficcaoCientifica: [
+    {
+      autor: 'Frank Herbert',
+      anoPublicaca: 1965,
+      titulo: 'Duna',
+    },
+    {
+      autor: 'Isaac Asimov',
+      anoPublicaca: 1951,
+      titulo: 'Fundação',
+    },
+  ],
 }
 
-console.log(ouraAndreInfo)
-
-// Acessar o conteudo de duas formas
-//    1 - com ponto (.)
-//    2 - com colquetes e o nome da propriedade (['<nome da propriedade>'])
-console.log(ouraAndreInfo.sleepEfficiency)
-console.log(ouraAndreInfo['totalSleep'])
-
-// Adicionar uma nova propriedade
-ouraAndreInfo.remSleep = '2h15m'
-console.log(ouraAndreInfo)
-
-// Alterar\Atualizar uma propriedade
-ouraAndreInfo.sleepScore = 90
-console.log(ouraAndreInfo)
-ouraAndreInfo['sleepScore'] = 91
-console.log(ouraAndreInfo)
-ouraAndreInfo.sleepScore++
-console.log(ouraAndreInfo)
-ouraAndreInfo['sleepScore'] += 4
-console.log(ouraAndreInfo)
-
-//Adicionando uma nova propriedade dentro de uma propriedade
-console.log(ouraAndreInfo.restinHeartRate.minHeartRate)
-console.log(ouraAndreInfo.restinHeartRate['maxHeartRate'])
-
-//Lendo um Array
-console.log(ouraAndreInfo.notes[0])
-console.log(ouraAndreInfo.notes[1])
+console.log(biblioteca)
