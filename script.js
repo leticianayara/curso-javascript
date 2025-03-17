3e75650619acbde8f07dce189348b792f0346bda
Leticia Costa <leticianayaralima@gmail.com>
2025-03-17 15:42:47 -0300

    Objetos

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
-//Desafio Operador ternário
+// Objects and Key Pairs (Dictionary)
+// Arrray = Objects
 
-let nota = 85
-let avaliacao =
-  nota >= 90
-    ? 'Excelente'
-    : nota >= 70
-    ? 'Bom'
-    : nota >= 50
-    ? 'Satisfatório'
-    : 'Insatisfatório'
+// let ouraAndreInfo2 = [9.7, 7.53, '86%', 59, 84]
 
-// if (nota >= 90) {
-//   avaliacao = 'Excelente'
-// } else if (nota >= 70) {
-//   avaliacao = 'Bom'
-// } else if (nota >= 50) {
-//   avaliacao = 'Satisfatório'
-// } else {
-//   avaliacao = 'Insatisfatório'
-// }
+const ouraAndreInfo = {
+  totalSleep: 7.53,
+  timeInBed: 9.7,
+  sleepEfficiency: '86%',
+  restinHeartRate: { maxHeartRate: 81, minHeartRate: 59 },
+  sleepScore: 84,
+  notes: ['Coffee', '30 Minutes reading'],
+}
 
-console.log(avaliacao) //Bom
+console.log(ouraAndreInfo)
+
+// Acessar o conteudo de duas formas
+//    1 - com ponto (.)
+//    2 - com colquetes e o nome da propriedade (['<nome da propriedade>'])
+console.log(ouraAndreInfo.sleepEfficiency)
+console.log(ouraAndreInfo['totalSleep'])
+
+// Adicionar uma nova propriedade
+ouraAndreInfo.remSleep = '2h15m'
+console.log(ouraAndreInfo)
+
+// Alterar\Atualizar uma propriedade
+ouraAndreInfo.sleepScore = 90
+console.log(ouraAndreInfo)
+ouraAndreInfo['sleepScore'] = 91
+console.log(ouraAndreInfo)
+ouraAndreInfo.sleepScore++
+console.log(ouraAndreInfo)
+ouraAndreInfo['sleepScore'] += 4
+console.log(ouraAndreInfo)
+
+//Adicionando uma nova propriedade dentro de uma propriedade
+console.log(ouraAndreInfo.restinHeartRate.minHeartRate)
+console.log(ouraAndreInfo.restinHeartRate['maxHeartRate'])
+
+//Lendo um Array
+console.log(ouraAndreInfo.notes[0])
+console.log(ouraAndreInfo.notes[1])
